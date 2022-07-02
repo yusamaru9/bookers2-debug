9862fff7b0b0f27ceeb9ea9919efe8204887d727
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-02 13:27:47 +0000

    [Update] 応用3 コメント機能view

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index

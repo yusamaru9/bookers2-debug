8448ce42bb836cd57a2834da7c8446df9f6427a4
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-03 10:44:41 +0000

    [Start] 応用4

--- bookers2-debug/db/schema.rb
+++ bookers2-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_123917) do
+ActiveRecord::Schema.define(version: 2022_07_03_094750) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2022_07_02_123917) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

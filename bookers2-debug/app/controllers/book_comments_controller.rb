73ae0d9d201ce01be378c8dbf116279da4a78846
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-07 10:22:22 +0000

    [Finish] 応用6 コメント機能非同期

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -3,13 +3,13 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
   
   private

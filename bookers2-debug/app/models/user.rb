32108e1bb43dc0d75ec256c3947e4f1f22c5aa83
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-10 03:51:29 +0000

    [Update] 投稿表示いいね多い順

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
-  has_many :favorites, dependent: :destroy
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy #dependent追加
+  has_many :liked_books, through: :favorites, source: :book
   has_many :book_comments, dependent: :destroy
   
   #フォローする、している側のユーザー

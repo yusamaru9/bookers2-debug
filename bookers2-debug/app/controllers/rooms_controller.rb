e39e27b08093bda2a4e2c658932528622ce12d35
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-07-13 14:55:13 +0000

    [Update] ルームコントローラ

--- bookers2-debug/app/controllers/rooms_controller.rb
+++ bookers2-debug/app/controllers/rooms_controller.rb
@@ -1,10 +1,13 @@
 class RoomsController < ApplicationController
   
-  
+  before_action :authenticate_user!
   
   
   def create
-    
+    @room = Room.create
+    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    redirect_to 
   end
   
   def show
